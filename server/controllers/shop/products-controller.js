3f8e9de8e5a3a1a600fa00976dd839fef6852d23
Srini-debug <srinivaasan_s@outlook.com>
2024-11-29 08:18:33 -0600

    sorting & filters shop listing page added

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -3,7 +3,42 @@ const Product = require("../../models/Product");
 // Implement filter functionality on the shopping page
 const getFilteredProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
+    const { category = [], brand = [], sortBy = "price-lowtohigh" } = req.query;
+
+    let filters = {};
+
+    if (category.length) {
+      filters.category = { $in: category.split(",") };
+    }
+
+    if (brand.length) {
+      filters.brand = { $in: brand.split(",") };
+    }
+
+    let sort = {};
+
+    switch (sortBy) {
+      case "price-lowtohigh":
+        sort.price = 1;
+        break;
+
+      case "price-hightolow":
+        sort.price = -1;
+        break;
+
+      case "title-atoz":
+        sort.title = 1;
+        break;
+
+      case "title-ztoa":
+        sort.title = -1;
+        break;
+
+      default:
+        sort.price = 1;
+    }
+
+    const products = await Product.find(filters).sort(sort);
 
     res.status(200).json({
       success: true,
