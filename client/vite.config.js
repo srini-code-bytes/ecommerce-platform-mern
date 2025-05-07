c5236d8d30a3e2cd3d86e22aa3c616b330bec3c8
Srinivaasan Swaminathan <srinivaasan_s@outlook.com>
2025-05-07 10:02:36 -0500

    frontend deployment prechecks

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,13 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // ✅ sets correct path for CloudFront/S3
   plugins: [react()],
-  // for shadcn
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
